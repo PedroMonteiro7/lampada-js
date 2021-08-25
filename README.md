4e2273859fdf118832874aee5f4ce4c47245ccdc
PedroMonteiro7 <21192977@CFP127ALUNO.LOCAL>
2021-08-25 11:06:41 -0300

    Readme pronto

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lampada-js
+Projeto criado com objetivo didático para as aulas de PWFE (Programação Web Front-End), no curso de Desenvolvimento de Sistemas do [SENAI Jandira](https://jandira.sp.senai.br/), sob a orientação do professor [Fernando Leonid](https://github.com/fernandoleonid).
+
+O projeto consiste em criar uma lâmpada com três ações:
+
+* Ligar;
+
+* Desligar;
+
+* Quebrar.
+
+Todo o código foi construído seguindo boas práticas, como responsabilidade única e funções puras.
+
+![](img/projeto.jpg)
--- lampada.js
+++ lampada.js
@@ -1,5 +1,16 @@
 "use strict"
 
+function id(elemento) {
+    return document.getElementById(elemento)
+}
+
+function botoesLigaDesliga(estadoLiga, estadoDesliga) {
+    const botaoLigar = document.getElementById('ligar')
+    const botaoDesligar = document.getElementById('desligar')
+    botaoLigar.disabled = estadoLiga
+    botaoDesligar.disabled = estadoDesliga
+}
+
 function lampadaQuebrada() {
     const lampada = document.getElementById('lampada')
     return lampada.src.indexOf('quebrada') !== -1 //indexOf procura uma String dentro de um caminho
@@ -7,38 +18,32 @@ function lampadaQuebrada() {
 
 function ligarLampada() {
     const lampada = document.getElementById('lampada')
-    const botaoLigar = document.getElementById('ligar')
-    const botaoDesligar = document.getElementById('desligar')
 
     if (!lampadaQuebrada()) {
     lampada.src = "img/ligada.jpg"
-    botaoLigar.disabled = true
-    botaoDesligar.disabled = false
+    botoesLigaDesliga(true, false)
     }
 }
 
 function desligarLampada() {
     const lampada = document.getElementById('lampada')
-    const botaoLigar = document.getElementById('ligar')
-    const botaoDesligar = document.getElementById('desligar')
 
     if (!lampadaQuebrada()) {
     lampada.src = "img/desligada.jpg"
-    botaoLigar.disabled = false
-    botaoDesligar.disabled = true
+    botoesLigaDesliga(false, true)
     }
 }
 
 function quebrarLampada() {
     const lampada = document.getElementById('lampada')
+
     lampada.src = "img/quebrada.jpg"
-    botaoLigar.disabled = true
-    botaoDesligar.disabled = true
+    botoesLigaDesliga(true, true)
 }
 
 // eventos
-document.getElementById('ligar').addEventListener('click', ligarLampada)
-document.getElementById('desligar').addEventListener('click', desligarLampada)
-document.getElementById('lampada').addEventListener('mouseover', ligarLampada)
-document.getElementById('lampada').addEventListener('mouseleave', desligarLampada)
-document.getElementById('lampada').addEventListener('dblclick', quebrarLampada)
+id('ligar').addEventListener('click', ligarLampada)
+id('desligar').addEventListener('click', desligarLampada)
+id('lampada').addEventListener('mouseover', ligarLampada)
+id('lampada').addEventListener('mouseleave', desligarLampada)
+id('lampada').addEventListener('dblclick', quebrarLampada)
